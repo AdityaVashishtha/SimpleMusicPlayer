1b448750278fad699d77f046511c4fbf13d88c2b
AdityaVashishtha <vashishthaaditya20@gmail.com>
2018-07-05 13:50:06 +0530

    time duration added

--- js/index.js
+++ js/index.js
@@ -3,27 +3,38 @@ var playPauseToggle = false;
 var playPauseButton = null;
 var loadingBar = null;
 var interval = null;
-var song = new Audio(CURRENT_SONG_URL);
+var loadedTimeElement = null;
+var remainingTimeElement = null;
 
+var song = new Audio(CURRENT_SONG_URL);
+song.preload = true;
 playPauseButton = document.getElementsByClassName("play-pause-btn")[0];
 progressBar = document.getElementsByClassName("progressbar")[0];
+loadedTimeElement = document.getElementsByClassName("loaded-time")[0];
+remainingTimeElement = document.getElementsByClassName("remaining-time")[0];
 
 playPauseButton.addEventListener('click',()=> {
+    togglePlayPauseBtn();
     if(playPauseToggle) {
         song.pause();
         if(interval != null)
             clearInterval(interval);
     } else {
+        //console.log(song.buffered.end(0));
         song.play();
         interval = setInterval(()=>{
-            console.log(song.currentTime);
-            //mySound.currentTime = 1;
-            console.log(song.duration);
             loadPercentage = (1-(song.duration - song.currentTime)/(song.duration))*100;
-            console.log(loadPercentage);
+            //console.log(loadPercentage);
+            let loadedTime = (( (song.currentTime / 60) < 10 ) ? "0"+Math.floor(song.currentTime/60) : ""+Math.floor(song.currentTime/60) ) +
+                               ":" + (( (song.currentTime % 60) < 9 ) ? "0"+Math.ceil(song.currentTime % 60) : ""+Math.ceil(song.currentTime % 60) ) ;
+            let remainSecond = song.duration - song.currentTime;
+            let remainingTime = (( (remainSecond / 60) < 10 ) ? "0"+Math.floor(remainSecond/60) : ""+Math.floor(remainSecond/60) ) +
+            ":" + (( (remainSecond % 60) < 9 ) ? "0"+Math.ceil(remainSecond % 60) : ""+Math.ceil(remainSecond % 60) ) ;
+
+            loadedTimeElement.innerHTML = loadedTime;
+            remainingTimeElement.innerHTML = remainingTime;
             document.getElementsByClassName("progress-bar-loading")[0].
-                style.width = loadPercentage+"%";
-            
+                style.width = loadPercentage+"%";            
         },1000);
     }
     playPauseToggle = !playPauseToggle;
@@ -38,7 +49,10 @@ progressBar.onclick = (e)=>{
     song.currentTime = song.duration * (clickPositionX/100);
 };
 
-// pauseButtom = document.getElementById("pause");
-// pauseButtom.addEventListener('click',()=> {
-//     mySound.pause();
-// });
+togglePlayPauseBtn = ()=>{
+    if(playPauseToggle) {
+        playPauseButton.innerHTML = "<i class='fa fa-play'></i>";
+    } else {
+        playPauseButton.innerHTML = "<i class='fa fa-pause'></i>";
+    }
+}
